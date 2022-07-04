8d9f72bddbf0fcf48a2c902251b6229398181dcf
slient13 <sjyhc25@gmail.com>
2022-07-04 20:44:41 +0900

    디자인 개선

--- ts/view/QuestionPanel.ts
+++ ts/view/QuestionPanel.ts
@@ -14,9 +14,18 @@ class QuestionPanel {
                 `<h1 class="title">${this.question.get_title()}</h1>` +
                 `<p class="contents">${this.question.get_contents()}</p>` +
                 `<div class="answer">` +
-                    `<p class="correct_answer">${this.question.get_answer(true)}</p>` +
-                    `<p class="user_answer">${this.question.get_answer(false)}</p>` +
-                    `<p class="is_correct">${this.question.check()}</p>` +
+                    `<div>` +
+                        `<h2 class="element_head">예상 출력</h2>` +
+                        `<p class="correct_answer">${this.question.get_answer(true)}</p>` +
+                    `</div>` + 
+                    `<div>` + 
+                        `<h2 class="element_head">실제 출력</h2>` +
+                        `<p class="user_answer">${this.question.get_answer(false)}</p>` +
+                    `</div>` + 
+                    `<div>` + 
+                        `<h2 class="element_head">정답 여부</h2>` +
+                        `<p class="is_correct">${this.question.check() ? "정답" : "오답"}</p>` +
+                    `</div>` + 
                 `</div>` +
             `</div>`
         );
